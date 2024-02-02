1e29617db650e69bef8e7505e381783a3c68a3e6
Simon Parten <Quafadas@users.noreply.github.com>
2024-02-02 15:07:36 +0100

    Create project.scala

---
+++ project.scala
@@ -0,0 +1,6 @@
+//> using dep com.disneystreaming.smithy:aws-textract-spec:2023.23.30
+//> using dep software.amazon.awssdk:textract:2.23.15
+//> using dep software.amazon.awssdk:bom:2.23.15
+//> using dep software.amazon.awssdk:sso:2.23.15
+//> using dep software.amazon.awssdk:s3:2.23.15
+//> using scala 3.3.1
